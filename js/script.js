4d50a7b954e4ab125fc9e1e7585bc5f5c492f408
anna-kramarova <kramarovaaa@gmail.com>
2017-05-12 19:47:34 +0300

    анимация, 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var showform = document.querySelector(".showform-btn");
 var searchform = document.querySelector(".hotel-search-form");
+var checkininput = searchform.querySelector("input");
 
 showform.addEventListener("click", function (evt) {
   evt.preventDefault();
   searchform.classList.toggle("hideform");
+  checkininput.focus();
 });
