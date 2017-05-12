7d93e64889367080de051757fcfc7084d1277176
anna-kramarova <kramarovaaa@gmail.com>
2017-05-12 21:00:14 +0300

    К получению допуска, 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var showform = document.querySelector(".showform-btn");
 var searchform = document.querySelector(".hotel-search-form");
 var checkininput = searchform.querySelector("input");
-
+searchform.classList.add("hideform");
 showform.addEventListener("click", function (evt) {
   evt.preventDefault();
   searchform.classList.toggle("hideform");
